bc0e3a9d42fa2c674d526e10d3a573a99725851b
ismael HADJ <ismael HADJ>
2017-10-31 15:04:07 +0100

    input + modal

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { AlertController } from 'ionic-angular';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,7 +10,7 @@ export class HomePage {
 
   app: AppModel;
 //app: any={nom: String, version: Number};
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
   //  this.app.nom="Titre01";
     //this.app.version=0.1;
     this.app = new AppModel("Titre001", 0.1);
@@ -19,6 +19,35 @@ export class HomePage {
 
   clickbutton(){
     console.log('click');
+  }
+
+
+  showPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Bonjour',
+      message: "Ceci est une modale avec un input",
+      inputs: [
+        {
+          name: 'Input',
+          placeholder: 'Inputplaceholder'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 
 }
